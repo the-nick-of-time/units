75e3feabb26c5332f33253f0148dd22c2b02e38e
Nick Thurmes <nthurmes@gmail.com>
2020-08-17 22:29:45 -0600

    Expand testing and exceptions

--- tests/test_length.py
+++ tests/test_length.py
@@ -15,3 +15,32 @@ class TestLength(TestCase):
         m = ft.to_meters()
         self.assertAlmostEqual(Feet.scale, m.value, places=4)
         self.assertIs(Meters, type(m))
+
+    def test_equal(self):
+        km_in_m = Meters(1000)
+        km = Kilometers(1)
+        self.assertEqual(km, km_in_m)
+
+    def test_add_same_unit(self):
+        a = Meters(1)
+        b = Meters(2)
+        expected = Meters(3)
+        self.assertEqual(expected, a + b)
+
+    def test_add_different_unit(self):
+        a = Meters(1)
+        b = Kilometers(1)
+        with self.assertRaises(OperationError):
+            a + b
+
+    def test_subtract_same_unit(self):
+        a = Meters(5)
+        b = Meters(2)
+        expected = Meters(3)
+        self.assertEqual(expected, a - b)
+
+    def test_subtract_different_unit(self):
+        a = Meters(1)
+        b = Kilometers(1)
+        with self.assertRaises(OperationError):
+            a - b
--- units/exceptions.py
+++ units/exceptions.py
@@ -1,4 +1,8 @@
-class OperationError(Exception):
+class UnitException(Exception):
+    pass
+
+
+class OperationError(UnitException):
     def __init__(self, operation, left, right):
         self.operation = operation
         self.left = left
@@ -6,3 +10,12 @@ class OperationError(Exception):
 
     def __str__(self):
         return f"Cannot {self.operation} {self.left} to {self.right}"
+
+
+class ImplicitConversionError(UnitException):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+    def __str__(self):
+        return f"Will not implicitly convert {self.source} to {self.dest}"
