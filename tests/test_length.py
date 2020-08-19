3a3db2f79d9e71f16460027e6b8b6e81213a3241
Nick Thurmes <nthurmes@gmail.com>
2020-08-18 22:58:34 -0600

    Make length follow the new convention

--- tests/test_length.py
+++ tests/test_length.py
@@ -1,46 +1,47 @@
 from unittest import TestCase
 
-from length import *
+from exceptions import OperationError
+from length import kilometers, meters, feet
 
 
 class TestLength(TestCase):
     def test_km_to_m(self):
-        km = Kilometers(1)
+        km = kilometers(1)
         m = km.to_meters()
-        self.assertEqual(Kilometers.scale, m.value)
-        self.assertIs(Meters, type(m))
+        self.assertEqual(kilometers.scale, m.value)
+        self.assertIs(meters, type(m))
 
     def test_ft_to_m(self):
-        ft = Feet(1)
+        ft = feet(1)
         m = ft.to_meters()
-        self.assertAlmostEqual(Feet.scale, m.value, places=4)
-        self.assertIs(Meters, type(m))
+        self.assertAlmostEqual(feet.scale, m.value, places=4)
+        self.assertIs(meters, type(m))
 
     def test_equal(self):
-        km_in_m = Meters(1000)
-        km = Kilometers(1)
+        km_in_m = meters(1000)
+        km = kilometers(1)
         self.assertEqual(km, km_in_m)
 
     def test_add_same_unit(self):
-        a = Meters(1)
-        b = Meters(2)
-        expected = Meters(3)
+        a = meters(1)
+        b = meters(2)
+        expected = meters(3)
         self.assertEqual(expected, a + b)
 
     def test_add_different_unit(self):
-        a = Meters(1)
-        b = Kilometers(1)
+        a = meters(1)
+        b = kilometers(1)
         with self.assertRaises(OperationError):
-            a + b
+            print(a + b)
 
     def test_subtract_same_unit(self):
-        a = Meters(5)
-        b = Meters(2)
-        expected = Meters(3)
+        a = meters(5)
+        b = meters(2)
+        expected = meters(3)
         self.assertEqual(expected, a - b)
 
     def test_subtract_different_unit(self):
-        a = Meters(1)
-        b = Kilometers(1)
+        a = meters(1)
+        b = kilometers(1)
         with self.assertRaises(OperationError):
-            a - b
+            print(a - b)
--- units/length.py
+++ units/length.py
@@ -1,54 +1,10 @@
 from decimal import Decimal
-from numbers import Number
 
-from exceptions import OperationError
+from base import make_unit, make_dimension
 
+Length = make_dimension("Length")
 
-class Length:
-    __slots__ = "value", "scale"
+meters = make_unit("meters", Length, 1)
 
-    def __init__(self, value):
-        if not isinstance(value, Number):
-            raise TypeError("A length is a scalar number")
-        self.value = value
-
-    def __add__(self, other):
-        if isinstance(other, type(self)):
-            return type(self)(self.value + other.value)
-        raise OperationError("add", type(self), type(other))
-
-    def __sub__(self, other):
-        if isinstance(other, type(self)):
-            return type(self)(self.value - other.value)
-        raise OperationError("add", type(self), type(other))
-
-    def __eq__(self, other):
-        if isinstance(other, Length):
-            return self._to_base_unit().value == other._to_base_unit().value
-
-    def _base_unit(self):
-        return Meters
-
-    def _to_base_unit(self):
-        return self._base_unit()(self.value * self.scale)
-
-    def to_meters(self):
-        return self._to_base_unit()
-
-    def to_kilometers(self):
-        return Kilometers(self._to_base_unit().value / self.scale)
-
-    def to_feet(self):
-        return Feet(self._to_base_unit().value / self.scale)
-
-
-class Meters(Length):
-    scale = Decimal("1")
-
-
-class Kilometers(Length):
-    scale = Decimal("1000")
-
-
-class Feet(Length):
-    scale = Decimal("0.3048")
+kilometers = make_unit("kilometers", Length, 1000)
+feet = make_unit("feet", Length, Decimal("0.3048"))
