e34ded87a75f4fb869983b160065db8006c10d9c
Nick Thurmes <nthurmes@gmail.com>
2022-09-26 00:56:10 -0600

    Introduce absolute zero to temperatures

---
+++ tests/test_temperature.py
@@ -0,0 +1,18 @@
+import pytest
+
+from units.exceptions import UnitException
+from units.temperature import celsius, celsius_to_kelvin_absolute, kelvin, fahrenheit
+
+
+def test_celsius_to_absolute():
+    c = celsius(25)
+    k = celsius_to_kelvin_absolute(c)
+
+    assert k == kelvin("298.15")
+
+
+def test_f_to_absolute():
+    f = fahrenheit(100)
+
+    with pytest.raises(UnitException):
+        celsius_to_kelvin_absolute(f)
--- units/temperature.py
+++ units/temperature.py
@@ -2,7 +2,15 @@
 
 from units.base import make_unit, make_dimension
 
-__all__ = ["Temperature", "kelvin", "celsius", "fahrenheit", "rankine"]
+__all__ = [
+    "Temperature",
+    "kelvin",
+    "celsius",
+    "fahrenheit",
+    "rankine",
+    "absolute_zero_celsius",
+    "celsius_to_kelvin_absolute",
+]
 
 Temperature = make_dimension("Temperature")
 
@@ -10,3 +18,9 @@ kelvin = make_unit("kelvin", Temperature, 1)
 celsius = make_unit("degrees celsius", Temperature, 1)
 fahrenheit = make_unit("degrees fahrenheit", Temperature, 5 / 9)
 rankine = make_unit("rankine", Temperature, 5 / 9)
+
+absolute_zero_celsius = celsius("-273.15")
+
+
+def celsius_to_kelvin_absolute(degc: celsius) -> kelvin:
+    return (degc - absolute_zero_celsius).to_kelvin()
