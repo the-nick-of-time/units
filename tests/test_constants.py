7c7aae3a23724279b172566153ef96006e7a429d
Nick Thurmes <nthurmes@gmail.com>
2022-09-27 18:26:21 -0600

    Some tests with complex generated converters

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,7 +1,19 @@
-from pyunitx.constants import g, R_E, M_E, G
+from pyunitx.constants import g, R_E, M_E, G, R
+from pyunitx.force import pounds
+from pyunitx.mass import av_pound
 
 
 def test_gravity_derivation():
     a = G * M_E / (R_E ** 2)
 
     assert a.equivalent_to(g, 0.1)
+
+
+def test_pound_mass():
+    assert (g.to_feet_per_second_squared() * av_pound(1)).equivalent_to(pounds(1), .001)
+
+
+def gas_constant_to_usc():
+    us = R.to_feet_pounds_per_rankine_per_slug()
+
+    assert us.sig_figs(4) == type(us)("49.72")
