3e8ef54fc7c32c30125fad79f762d98ed4c72bf2
Nick Thurmes <nthurmes@gmail.com>
2022-10-29 00:27:08 -0600

    Read a resistor color code
    the color symbols aren't very intuitive but there are green & gray as well as brown, black, blue

--- pyunitx/resistance.py
+++ pyunitx/resistance.py
@@ -18,8 +18,41 @@ ohms = make_compound_unit(
 generated = si_unit(base_unit=ohms)
 globals().update(generated)
 
+
+def from_color(spec: str) -> ohms:
+    values = {
+        "K": 0,
+        "B": 1,
+        "R": 2,
+        "O": 3,
+        "Y": 4,
+        "E": 5,
+        "U": 6,
+        "V": 7,
+        "G": 8,
+        "W": 9,
+    }
+    multipliers = {
+        "K": 10 ** 0,
+        "B": 10 ** 1,
+        "R": 10 ** 2,
+        "O": 10 ** 3,
+        "Y": 10 ** 4,
+        "G": 10 ** 5,
+        "U": 10 ** 6,
+        "V": 10 ** 7,
+    }
+    value = 0
+    spec = spec.upper()
+    for i, char in enumerate(spec[-2::-1]):
+        value += values[char] * 10 ** i
+    value *= multipliers[spec[-1]]
+    return ohms(value)
+
+
 __all__ = [
     "Resistance",
     "ohms",
+    "from_color",
     *generated.keys()
 ]
