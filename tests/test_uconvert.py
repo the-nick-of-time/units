ceb6a13159e4fc3b6cf64a705dcd388ec5474e67
Nick Thurmes <nthurmes@gmail.com>
2022-11-01 17:53:17 -0600

    Check that inconsistent units are definable

--- tests/test_uconvert.py
+++ tests/test_uconvert.py
@@ -3,7 +3,8 @@ import sys
 import pytest
 
 from pyunitx._api import Compound
-from pyunitx.length import meters
+from pyunitx.area import acres
+from pyunitx.length import meters, feet
 from pyunitx.mass import kilograms
 from pyunitx.time import seconds
 from pyunitx.uconvert import parse_unit, main
@@ -18,6 +19,8 @@ from pyunitx.volume import fluid_ounces
         ("kg.s^-1", ((kilograms, 1), (seconds, -1))),
         ("kg/s", ((kilograms, 1), (seconds, -1))),
         ("kg/s.m", ((kilograms, 1), (seconds, -1), (meters, 1))),
+        ("m/s/s", ((seconds, -2), (meters, 1))),
+        ("acre.ft", ((acres, 1), (feet, 1))),
     ]
 )
 def test_parse_unit_success(spec, expected):
