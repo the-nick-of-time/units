de0465c1f0d533cd30e49b7340263a0b3e4aead9
Nick Thurmes <nthurmes@gmail.com>
2022-11-01 14:00:02 -0600

    Verify both flags equivalent to just the broadest

--- tests/test_resistor.py
+++ tests/test_resistor.py
@@ -12,6 +12,7 @@ from pyunitx.resistor import main
         [["ook"], "33 Ω"],
         [["-t", "ook"], "33 Ω ± 6.6 Ω"],
         [["-s", "rgokvv"], "283 Ω + 0.001415 Ω/K ± 283 mΩ"],
+        [["-st", "rgokvv"], "283 Ω + 0.001415 Ω/K ± 283 mΩ"],
     ]
 )
 def test_main(monkeypatch, capsys, args, expected):
