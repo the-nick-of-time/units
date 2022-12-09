6f6b8211d1471cdf9b3108795828c29d94bdc33b
Nick Thurmes <nthurmes@gmail.com>
2022-12-09 16:11:01 -0700

    Test most recent code path

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,3 +1,4 @@
+from pyunitx._api import make_compound_unit
 from pyunitx.frequency import hertz, kilohertz
 from pyunitx.length import kilometers, feet, meters
 from pyunitx.time import seconds, hours, minutes, milliseconds
@@ -25,3 +26,21 @@ def test_rdiv():
     t = milliseconds(2)
 
     assert (4 / t).to_kilohertz() == kilohertz(2)
+
+
+def test_decimal_non_si():
+    new = make_compound_unit(name="new", scale="1e5", abbrev="n", exponents={seconds: 1})
+
+    assert seconds(1000).to_new() == new("0.01")
+
+
+def test_non_decimal():
+    new = make_compound_unit(name="new2", scale="72", abbrev="n", exponents={seconds: 1})
+
+    assert seconds(144).to_new2() == new(2)
+
+
+def test_fake_si():
+    new = make_compound_unit(name="new3", scale="1e6", abbrev="n", exponents={feet: 1})
+
+    assert meters(1000).to_new3() == new(".001")
